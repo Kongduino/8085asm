5cab2f41108b3f710f7e22a9c655a791af9f50b8
Kongduino <dbvic@me.com>
2022-09-04 00:51:08 +0800

    Added Function labels

--- 8085dasm.c
+++ 8085dasm.c
@@ -1,115 +1,95 @@
 /* ########################################################################
-
    8085asm - Simple 8085 assembler and disassembler
-
    ########################################################################
-
-   Copyright (c) : 2009  Luis Claudio Gambôa Lopes
-
+   Copyright (c) : 2009  Luis Claudio GambÃ™a Lopes
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
    For e-mail suggestions :  lcgamboa@yahoo.com
    ######################################################################## */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<stddef.h>
-#include<ctype.h>
-
-#include"8085asm.h"
-#include"opcodes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <ctype.h>
+#include "8085asm.h"
+#include "hex2label.h"
 
 extern int read_ihx(const char * fname);
 extern int mc;
 
-int
-main(int argc,char** argv)
-{
-char fname[256];
-int i,j,k;
-unsigned short paddr;
-
-  if(argc == 2)
-  {
-    strcpy(fname,argv[1]);
-  }  
-  else
-  {
-  printf("enter the file name: ");
-  scanf("%s",fname);
+int main(int argc, char** argv) {
+  char fname[256];
+  FILE* fin;
+  int i, j, found;
+  unsigned short paddr;
+  if (argc == 2) {
+    strcpy(fname, argv[1]);
+  } else {
+    printf("enter the file name: ");
+    scanf("%s", fname);
   }
-
+  fin = fopen(fname, "r");
+  if (!fin) {
+    printf("Error open file:%s\n", fname);
+    return -1;
+  }
   printf(";LCGamboa 8085 disassembler 2008\n\n");
-
   read_ihx(fname);
-
-
-  printf("         ORG %04X\n",mem[0].addr); 
-  paddr=mem[0].addr; 
-
-  for(i=0;i<mc;i++)
-  {
+  printf("         ORG %04X\n", mem[0].addr);
+  paddr = mem[0].addr;
+  for (i = 0; i < mc; i++) {
     //printf("%04X  %02X\n",mem[i].addr,mem[i].value);
-      
-    if(mem[i].addr != paddr)
-    {
-      printf("         ORG %04X\n",mem[i].addr); 
-      paddr=mem[i].addr; 
+    if (mem[i].addr != paddr) {
+      printf("         ORG %04X\n", mem[i].addr);
+      paddr = mem[i].addr;
     }
-    
-    j=0;
-    do
-    {
-
-      if(opcode[j].opc == mem[i].value)
-      {
-        printf("L%04X:   %s ",mem[i].addr,opcode[j].men); 
-	
-	if( opcode[j].nargs == 1)
-	{
-	  if(opcode[j].uargs ==0)
-	    printf("%s ",opcode[j].arg1);
-	  else
-	    printf("%s, ",opcode[j].arg1);
-	}
-	
-	if( opcode[j].nargs == 2)
-	{
-	  printf("%s, %s ",opcode[j].arg1, opcode[j].arg2); 
-	}
-        
-	for(k=opcode[j].uargs; k >0 ; k--)
-	{
-	  if(k==1)	
-	    printf("%02Xh",mem[i+k].value); 
-	  else
-	    printf("%02X",mem[i+k].value); 
-	}
-	
-	printf("\n");
-        
-	i+=opcode[j].uargs;
-	paddr+=opcode[j].uargs+1;
-	break;
+    j = 0;
+    do {
+      if (opcode[j].opc == mem[i].value) {
+        printf("L%04X:   %s ", mem[i].addr, opcode[j].men);
+        if (opcode[j].nargs == 1) {
+          if (opcode[j].uargs == 0) printf("%s ", opcode[j].arg1);
+          else printf("%s, ", opcode[j].arg1);
+        }
+        if (opcode[j].nargs == 2) {
+          printf("%s, %s ", opcode[j].arg1, opcode[j].arg2);
+        }
+        if(opcode[j].uargs > 0) {
+          if (opcode[j].uargs == 2) {
+            char tmp[10];
+            sprintf(tmp, "0x%02X%02X", mem[i + 2].value, mem[i + 1].value);
+            unsigned int v = mem[i + 2].value * 256 + mem[i + 1].value;
+            found = -1;
+            for (int ix = 0; ix < numLabels; ix ++) {
+              if(mylabels[ix].value == v) {
+                printf("%s [%s]", mylabels[ix].name, tmp);
+                found = 0;
+                ix = numLabels + 1;
+              }
+            }
+            if (found == -1) printf("%s", tmp);
+          } else {
+            printf("0x%02X", mem[i + 1].value);
+          }
+        }
+        printf("\n");
+        i += opcode[j].uargs;
+        paddr += opcode[j].uargs + 1;
+        break;
       }
     }
-    while(strcmp(opcode[j++].men,"ENDO") != 0);
+    while (strcmp(opcode[j++].men, "ENDO") != 0);
   }
-
-
-return 1;
+  return 1;
 }
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-
 # 8085asm - Simple 8085/8080 assembler and disassembler
 
-Simple 8085 assembler and disassembler for didatic use.
-The 8085asm generate the map file and intel hex file. 
-Support the directives EQU, ORG, DB, DS and END.
+Simple 8085 assembler and disassembler for didatic use. `8085asm` generates the map file and intel hex file. It supports the EQU, ORG, DB, DS and END directives.
 
-The 8085toZ80 application is a translator of 8085asm(8080) to z80asm.  
+The 8085toZ80 application is a translator of 8085asm(8080) to z80asm.
 
+## The Fork
+
+I have forked this repository to improve and add to the assembler and disassembler. The first thing I did was to add the name of known (to me at least, there may be more) ROM functions and useful variables. There's a `header.asm` file with lots of `EQU`s, which will help you make your code more readable. Similarly, there's a `hex2label.h` include file for the disassembler, and the code has been updated to make use of the function labels:
+
+![labels](img/labels.png)
+
+I am planning to rework the code and make it – possibly – into a GUI app, backed by the 2 command-line applications. So... WIP.
 
